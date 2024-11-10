e633826b23dde2edb34d8c789464b9f0fc3536d0
Anthony Igwe <igweanthony15@gmail.com>
2024-11-10 17:51:36 -0500

    Commit changes in submodule

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './router'
 
-axios.defaults.baseURL = "PASTE API GATEWAY ENDPOINT HERE"
+axios.defaults.baseURL = " https://rt1tdvcy37.execute-api.us-east-1.amazonaws.com/v1/"
 
 createApp(App).use(router).mount('#app')
